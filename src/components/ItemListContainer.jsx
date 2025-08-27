c552009d7968cd2ba18ed32ef6d078b82165cf6f
Bartender1991 <opascualetti21@gmail.com>
2025-08-27 01:58:22 -0300

    .

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,5 +1,17 @@
+import { useState } from "react"
+import { getProducts } from "../mock/AsyncMock"
+
 const ItemListContainer = (props) => {
-    const {mensaje} = props
+    const { mensaje } = props
+    const [data, SetData] = useState([])
+
+    useEffect(() => {
+        getProducts()
+            .then((res) => SetData(res))
+            .catch((error) => console.error(error))
+    }, [])
+    console.log(data)
+
     return (
         <div>
             <h1>{mensaje}</h1>
