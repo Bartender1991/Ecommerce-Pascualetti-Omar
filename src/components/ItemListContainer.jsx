272a222d430a495418e37308618ca700c43ccaad
Bartender1991 <opascualetti21@gmail.com>
2025-09-15 21:48:01 -0300

    solvend errors

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -5,7 +5,7 @@ import { useParams } from "react-router-dom"
 import LoaderComponent from "./LoaderComponent"
 import { collection, getDocs, query, where } from "firebase/firestore"
 import { db } from "../service/firebase"
-import SubirMasivo from "./subirMasivo"
+import SubirMasivo from "./SubirMasivo"
 
 const ItemListContainer = (props) => {
     const [data, SetData] = useState([])
